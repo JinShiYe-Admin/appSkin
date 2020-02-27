c7ba4081602890d20ec4db04b2e45be60717081b
LuckyCX <721685743@qq.com>
2020-02-27 14:52:42 +0800

    考勤模块

--- js/utils/storageKeyName.js
+++ js/utils/storageKeyName.js
@@ -50,6 +50,7 @@ var storageKeyName = (function(mod) {
 			mod.INTERFACE_OA = 'http://nwifapp.jiaobaowang.net/oasubapi/sub/api/';//oa接口
 			mod.INTERFACE_BASESUB = 'http://nwifapp.jiaobaowang.net/testbaseapi/api/Data/';//统一信息接口
 			mod.INTERFACE_EXAMINATION = 'http://139.129.252.49:8180/stuzy/api/paper/';//考务系统接口
+			mod.INTERFACE_WORK = 'http://139.129.252.49:8180/stukqsubapi/attendance/';//考勤系统接口
 			
 			//云测 
 			// mod.INTERFACE_SSO_SKIN = 'http://nwifapp.jiaobaowang.net/suptohr/api/skin/'; //单点登录,供PC外皮及APP用户登录,管理  单点对人事及登录皮肤接口
@@ -60,6 +61,7 @@ var storageKeyName = (function(mod) {
 			// mod.INTERFACE_OA = 'http://nwifapp.jiaobaowang.net/oasubapi/sub/api/';//oa接口
 			// mod.INTERFACE_BASESUB = 'http://nwifapp.jiaobaowang.net/baseapi/api/Data/';//统一信息接口:考务用
 			// mod.INTERFACE_EXAMINATION = 'http://nwifapp.jiaobaowang.net/stuzy/api/paper/';//考务系统接口
+			// mod.INTERFACE_WORK = 'http://139.129.252.49:8180/stukqsubapi/attendance/';//考勤系统接口
 			
 			
 			// mod.INTERFACE_SSO_SKIN = 'http://139.129.252.49:8080/sup2/api/skin/'; //单点登录,供PC外皮及APP用户登录,管理
