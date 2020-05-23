efddcd701165358965e13d287fa2850643b386fa
Zqw <578159259@qq.com>
2020-05-23 10:05:20 +0800

    家校互动选年级、班级、学生修改

--- js/utils/mui.js
+++ js/utils/mui.js
@@ -7213,9 +7213,9 @@ Function.prototype.bind = Function.prototype.bind || function(to) {
 
 			if (!classList.contains(CLASS_ACTIVE)) { //展开时,需要收缩其他同类
 				var collapse = cell.parentNode.querySelector('.mui-collapse.mui-active');
-				if (collapse&& !classList.contains("keep-unfold")) {
-					collapse.classList.remove(CLASS_ACTIVE);
-				}
+				// if (collapse&& !classList.contains("keep-unfold")) {
+				// 	collapse.classList.remove(CLASS_ACTIVE);
+				// }
 				isExpand = true;
 			}
 			classList.toggle(CLASS_ACTIVE);
